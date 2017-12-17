915ad747aae82f040191980d36d1ebd4415c70c3
Emran Ahmed <emran.bd.08@gmail.com>
2017-12-18 04:41:11 +0600

    Simplified

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,7 +1,5 @@
 jQuery($ => {
     import('./FVSCustomVariationInput').then(() => {
-        $('.variations_form').FVSCustomVariationInput();
-
         // Init on Ajax Popup :)
         $(document).on('wc_variation_form', '.variations_form', function () {
             $(this).FVSCustomVariationInput();
